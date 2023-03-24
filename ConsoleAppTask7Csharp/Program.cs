874d66109f13cd4be24bc91cda2d3c36c4ef6d55
ElWin95 <elvinma@code.edu.az>
2023-03-24 18:12:11 +0400

    'task1'

--- ConsoleAppTask7Csharp/Program.cs
+++ ConsoleAppTask7Csharp/Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Task1
+            /* 1) Verilmish metnde {a} simvolun sayi {b} simvolun sayinda nece defe coxdur?
+             */
+
+            //Console.Write("Metni daxil edin: ");
+            //string text = Console.ReadLine();
+
+            //Console.Write("A simvolunu daxil edin: ");
+            //char a = Console.ReadKey().KeyChar;
+
+            //Console.Write("\nB simvolunu daxil edin: ");
+            //char b = Console.ReadKey().KeyChar;
+
+            //int aCount = 0, bCount = 0;
+
+            //foreach (char c in text)
+            //{
+            //    if (c == a)
+            //    {
+            //        aCount++;
+            //    }
+            //    else if (c == b)
+            //    {
+            //        bCount++;
+            //    }
+            //}
+
+            //if (aCount > bCount)
+            //{
+            //    Console.WriteLine("\n{0} simvolunun sayi {1} simvolunun sayindan {2} defe coxdur.", a, b, aCount - bCount);
+            //}
+            //else if (bCount > aCount)
+            //{
+            //    Console.WriteLine("\n{0} simvolunun sayi {1} simvolunun sayindan {2} defe coxdur.", b, a, bCount - aCount);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("\n{0} simvolunun sayi {1} simvolunun sayina beraberdir.", a, b);
+            //}
+            #endregion
         }
     }
 }
